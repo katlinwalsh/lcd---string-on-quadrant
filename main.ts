ee063f4936086bbbc43a55a45aa37ffd09a65128
Katlin Walsh <walskatl@sheridancollege.ca>
2026-05-11 16:18:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+/**
+ * Modify & Create: If you press a button, move the message to a new quadrant.
+ */
+fwdSensors.initializeLcd()
+/**
+ * On quadrant 1, display the text: "Quad 1", repeat for quadrants 2, 3, 4.
+ * 
+ * If your string is too long, ">8 chars" will display instead of your text.
+ */
+basic.forever(function () {
+    fwdSensors.lcd1.printQuadrantString("Quad 1", 1)
+    fwdSensors.lcd1.printQuadrantString("Quad 2", 2)
+    fwdSensors.lcd1.printQuadrantString("Quad 3", 3)
+    fwdSensors.lcd1.printQuadrantString("Quad 4", 4)
+})
